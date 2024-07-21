3525fbb494ac8d749c31322819cff66a760546a4
ChangHoMoon <tony9911@naver.com>
2024-07-21 18:06:31 +0900

    [feat] BaseEntity 생성

---
+++ src/main/java/com/example/myboard/MyBoardApplication.java
@@ -0,0 +1,14 @@
+package com.example.myboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class MyBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBoardApplication.class, args);
+    }
+}
