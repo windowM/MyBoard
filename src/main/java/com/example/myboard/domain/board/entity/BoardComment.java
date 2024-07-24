40d0576c3dd42fa87b6aa2bfe66ffda4aa23c067
ChangHoMoon <tony9911@naver.com>
2024-07-24 14:01:28 +0900

    [feat] boardImage, boardComment 엔티티에 BaseEntity상속

--- src/main/java/com/example/myboard/domain/board/entity/BoardComment.java
+++ src/main/java/com/example/myboard/domain/board/entity/BoardComment.java
@@ -1,5 +1,6 @@
 package com.example.myboard.domain.board.entity;
 
+import com.example.myboard.global.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,12 +8,13 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
-public class BoardComment {
+public class BoardComment extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String commentMemberEmail;
     private String ParentMemberEmail;
     private String comment;
+    @Column(nullable = false, name = "board_id")
     private Integer boardId;
 }
--- src/main/java/com/example/myboard/domain/board/entity/BoardImage.java
+++ src/main/java/com/example/myboard/domain/board/entity/BoardImage.java
@@ -1,5 +1,6 @@
 package com.example.myboard.domain.board.entity;
 
+import com.example.myboard.global.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,12 +8,13 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
-public class BoardImage {
+public class BoardImage extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String originalName;
     private String saveName;
+    @Column(nullable = false, name = "board_id")
     private Integer boardId;
 }
